16621150bee9f1e714e18a05f35c5786418f71e8
Akshat Pradhan <akshat.ip@gmail.com>
2012-04-29 06:35:04 -0400

    another changed to keys to get it working with heroku

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => 'AKIAIEG7X6JIVLEIZFPA',
-  :secret_access_key => 'AjJ1E+etehxbCdSfKJkNG2c9z9xdQmMU8OihEUxbpEdY'
+  :access_key_id     => 'AKIAJGOTIHHLWC7VSJPQ',
+  :secret_access_key => '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'
